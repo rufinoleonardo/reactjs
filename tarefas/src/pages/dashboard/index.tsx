1a8bc3b2dc576c7541f65ec02bc8f31f3821722e
Leonardo <rufinodevelopment@gmail.com>
2024-05-29 22:50:16 -0300

    Creating the Dashboard's Form and the component TextArea

--- tarefas/src/pages/dashboard/index.tsx
+++ tarefas/src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from 'next';
 import styles from './styles.module.css';
 import Head from 'next/head';
 import { getSession } from 'next-auth/react';
+import { TextArea } from '@/components/textarea';
 
 export default function Dashboard() {
     return (
@@ -10,7 +11,25 @@ export default function Dashboard() {
                 <title>Meu painel de tarefas</title>
             </Head>
 
-            <h1>Página painel</h1>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>
+                            Qual a sua próxima tarefa?
+                        </h1>
+                        <form>
+                            <TextArea placeholder='Digite aqui a sua tarefa' />
+                            <div className={styles.checkboxArea}>
+                                <input type="checkbox" name="publicTask" id="publicTask" className={styles.checkbox} />
+                                <label htmlFor="publicTask">Marcar como Tarefa pública</label>
+                            </div>
+                            <button type="submit" className={styles.button}>
+                                Registrar
+                            </button>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
